8ac960e97b4d6ce2c6f97db4cb85c2f464b193b1
fumi hagihara <7676.fmhghr@gmail.com>
2021-10-30 22:32:51 +0900

    rails g controller sessionsでセッションの作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 end
